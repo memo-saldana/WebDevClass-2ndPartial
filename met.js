3dcb352a645f601fb04aa17b0d728f96da201553
Jose Guillermo Saldana <memo.sal98@gmail.com>
2019-10-21 17:06:44 -0500

    Removes unnecessary error if and adds json parsing option

--- met.js
+++ met.js
@@ -3,26 +3,24 @@ const request = require('request'),
 
 exp.search = search => {
   return new Promise((resolve, reject) => { // Extra point for promises?
-    request.get(`https://collectionapi.metmuseum.org/public/collection/v1/search?q=${search}`,(error, response, body)=>{
+    request.get({url:`https://collectionapi.metmuseum.org/public/collection/v1/search?q=${search}`, json:true} ,(error, response, body)=>{
       if(error) return reject({requestError: true, error});
-      let jsonBody = JSON.parse(body);
-      if(jsonBody.total==0) return reject({requestError: false, message: "No se encontró ningun resultado con la busqueda", statusCode: 404});
-      return resolve( jsonBody.objectIDs[0]);
+      if(body.total==0) return reject({requestError: false, message: "No se encontró ningun resultado con la busqueda", statusCode: 404});
+      return resolve( body.objectIDs[0]);
     })
   });
 }
 
 exp.get = objectId => {
   return new Promise((resolve, reject) => {
-    request.get(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${objectId}`, (error, response, body) => {
+    request.get({url:`https://collectionapi.metmuseum.org/public/collection/v1/objects/${objectId}`, json:true}, (error, response, body) => {
       if(error) return reject({requestError: true, error})
-      let jsonBody = JSON.parse(body);
       return resolve({
-        artist : jsonBody.constituents[0].name,
-        title: jsonBody.title,
-        year: jsonBody.objectEndDate,
-        technique: jsonBody.medium,
-        metUrl: jsonBody.objectURL
+        artist : body.constituents[0].name,
+        title: body.title,
+        year: body.objectEndDate,
+        technique: body.medium,
+        metUrl: body.objectURL
       }
       )
     })
